77dbeb9eb75fb1b76e244b6059ae1d7c060e9658
Vlad Nikandrov <mouceme@gmail.com>
2025-05-05 22:29:30 +0400

    finish project, try for review, try 1

---
+++ blogicum/blog/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+
+from .models import Category, Location, Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'pub_date',
+        'author',
+        'location',
+        'category',
+        'is_published'
+    )
+    list_editable = (
+        'category',
+        'is_published'
+    )
+    search_fields = ('title',)
+    list_filter = ('category',)
+    list_display_links = ('title',)
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category)
+admin.site.register(Location)
